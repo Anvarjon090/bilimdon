e2334b48bf02b1045e0659442809a54a24875154
Anvarjon090 <anvarjonnizomiddinov7@gmail.com>
2025-04-04 17:32:12 +0500

    bilimdon

--- app/deppendencies.py
+++ app/deppendencies.py
@@ -1,12 +1,12 @@
 from typing import Annotated
-from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.orm import sessionmaker, Session, SessionLocal
 from sqlalchemy.ext.declarative import declarative_base  
 from fastapi import Depends
 
 Base = declarative_base()  
 
 def get_db():
-    db = SessionLocal()  # type: ignore
+    db = SessionLocal()  
     try:
         yield db
     finally:
