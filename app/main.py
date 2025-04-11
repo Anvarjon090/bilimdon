d09008a94a58debd7698166c19dc87a5e1a3964a
Anvarjon090 <anvarjonnizomiddinov7@gmail.com>
2025-04-11 17:30:53 +0500

    Yangi

--- app/main.py
+++ app/main.py
@@ -2,13 +2,13 @@ from typing import Union
 import time
 from datetime import datetime
 
-from fastapi import APIRouter, FastAPI, Request
+from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 
-# from app.routers.authr import router as auth_router
-
-
-router = APIRouter()
+from app.routers.authr import router as auth_router
+from app.routers.question import router as question_router
+from app.routers.topic import router as topic_router
+from app.routers.options import router as option_router
 
 app = FastAPI()
 
@@ -18,4 +18,7 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-# app.include_router(auth_router)   
+app.include_router(auth_router)
+app.include_router(question_router)
+app.include_router(topic_router)
+app.include_router(option_router)
---
+++ app/schemas/model_schema.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+class QuestionRequest(BaseModel):
+    owner_id: int
+    title: str
+    description:str
+    topic_id:int
+   
+
+
+class QuestionResponse(BaseModel):
+    id:int
+    owner_id: int
+    title: str
+    description:str
+    topic_id:int
+    created_at:datetime
+   
+
+
+
+
+class TopicRequest(BaseModel):
+    name:str
+
+
+
+class OptionRequest(BaseModel):
+    question_id: int
+    title: str
+    is_correct:bool
+   
+   
+
+
+class OptionResponse(BaseModel):
+    id:int
+    question_id: int
+    title: str
+    is_correct:str
+    created_at:datetime
+  
